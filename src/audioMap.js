b4307b0c0c80b20ba6f60662aa25e1dca59bbed2
KevinAnggrek <kevin.anggrek@gmail.com>
2024-06-25 21:52:07 +0800

    feat: Add universal source files used throughout the project

---
+++ src/audioMap.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const audioMap = {
+    'ara-ara': path.join(__dirname, '..','resources', 'audio', 'ara-ara.mp3'),
+    'ara-ara2': path.join(__dirname, '..','resources', 'audio', 'ara-ara-sayonara-mp3cut.mp3'),
+    'yntkts': path.join(__dirname, '..','resources', 'audio', 'yntkts-sound-effect.mp3'),
+    'duarmemek': path.join(__dirname, '..','resources', 'audio', 'duar.mp3'),
+    'soeharto': path.join(__dirname, '..','resources', 'audio', 'KENAPA KAMU TANYA BEGITU ANAK SD  SOEHARTO_Audio Trimmer.mp3')
+};
+
+module.exports = audioMap;
